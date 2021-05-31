f5dd40c0fed3c78378909362b61fb8838f88f5ac
Aakash Garg <aakashgarg2800@gmail.com>
2021-05-31 12:54:15 +0530

    Fixed Search

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var dropdown = document.querySelectorAll('.dropdown-menu')[1];
 var dropbtn = document.querySelectorAll('.dropdown-toggle')[1];
-var input = document.querySelector('.form-control');
+var input = document.querySelectorAll('.form-control')[2];
 dropdown.addEventListener('click',(e) => {
   console.log(dropbtn);
   dropbtn.innerHTML = e.target.innerText;
